6149131cc77f37f78edee425f55f6988f5c81a9f
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2025-05-04 12:29:57 +0400

    Initial commit

---
+++ src/test/java/turing/edu/az/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package turing.edu.az;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
